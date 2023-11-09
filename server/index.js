a84097c1011606523e4a6bcfff85cc75b088ba30
Harsh Sangrulkar <120011079+HarshSangrulkar@users.noreply.github.com>
2023-11-10 02:40:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+//import dotenv from 'dotenv';
+//dotenv.config();
+//process.env.MONGO
+//MONGO = "mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect("mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("Connected to MongoDB");
 }).catch((err)=>{
     console.log(err);
