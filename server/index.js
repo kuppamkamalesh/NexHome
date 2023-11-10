8c3e7a5332de0d64274d54fd09f1d16950158663
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2023-11-10 11:50:26 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
 import express from "express";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 //import dotenv from 'dotenv';
 //dotenv.config();
 //process.env.MONGO
 //MONGO = "mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority"
-import userRouter from './routes/user.route.js';
+import userRouter from "./routes/user.route.js";
 
-
-mongoose.connect("mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+mongoose
+  .connect(
+    "mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 const app = express();
 
-app.listen(4000,()=>{
-    console.log("Server running on port 4000");
-})
+app.listen(5000, () => {
+  console.log("Server running on port 4000");
+});
 
-app.use('/server/user', userRouter);
+app.use("/server/user", userRouter);
