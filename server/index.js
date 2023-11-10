6bac4f3321db02294afcaa2ffe41d1bfb991cedc
Harsh Sangrulkar <120011079+HarshSangrulkar@users.noreply.github.com>
2023-11-10 06:01:27 +0000

    created server/user/test route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 //dotenv.config();
 //process.env.MONGO
 //MONGO = "mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority"
+import userRouter from './routes/user.route.js';
+
 
 mongoose.connect("mongodb+srv://harshsangrulkar:1234@cluster0.8ptiwxj.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("Connected to MongoDB");
@@ -16,3 +18,5 @@ const app = express();
 app.listen(4000,()=>{
     console.log("Server running on port 4000");
 })
+
+app.use('/server/user', userRouter);
